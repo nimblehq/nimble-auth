eb495a07b60d50a247ea0d8b8d8775c6e127373e
Abhinav Mishra <makeawish880@gmail.com>
2018-02-15 17:43:14 +0700

    Fixed the failing route specs.

--- spec/dummy/config/initializers/buriauth.rb
+++ spec/dummy/config/initializers/buriauth.rb
@@ -1,7 +1,7 @@
 BuriAuth.setup do |config|
   config.resource_class = 'User'
   config.resource_identity_class = 'Identity'
-  config.omniauth_providers = %i[facebook].freeze
+  config.omniauth_providers = %i[facebook google_oauth2].freeze
   config.facebook_app_id = ENV['FACEBOOK_APP_ID']
   config.facebook_app_secret = ENV['FACEBOOK_APP_SECRET']
   config.google_app_id = ENV['FACEBOOK_APP_ID']
