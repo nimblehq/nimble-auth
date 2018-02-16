b8ba3f78c36a975e072b8c686806ca6b8cc973a1
Abhinav Mishra <makeawish880@gmail.com>
2018-02-16 15:58:14 +0700

    Fixed the failing specs.

--- spec/dummy/config/initializers/buriauth.rb
+++ spec/dummy/config/initializers/buriauth.rb
@@ -2,8 +2,8 @@ BuriAuth.setup do |config|
   config.resource_class = 'User'
   config.resource_identity_class = 'Identity'
   config.omniauth_providers = %i[facebook google_oauth2].freeze
-  config.facebook_app_id = "19312345674803053449100"
-  config.facebook_app_secret = "81b12345d21a5cbba7b0da39a03792b3fa858"
-  config.google_app_id = "2320364512345677326-n1qhbva17sua1q727jnb99prvo5inie1.apps.googleusercontent.com"
-  config.google_app_secret = "Lp6oWqwertyuiGjNSn9EPOU0QWCYpmj_"
+  config.facebook_app_id = "facebook_app_id"
+  config.facebook_app_secret = "facebook_app_secret"
+  config.google_app_id = "google_app_id"
+  config.google_app_secret = "google_app_secret"
 end
